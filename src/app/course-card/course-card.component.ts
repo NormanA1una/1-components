9e446272668ab72b6bb094511d3efb153f611934
NormanA1una <baciloferoz@gmail.com>
2023-04-19 16:05:01 -0600

    first commit 1-components

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from "@angular/core";
+import { COURSES } from "src/db-data";
+import { Course } from "../model/course";
+
+@Component({
+  selector: "course-card",
+  templateUrl: "./course-card.component.html",
+  styleUrls: ["./course-card.component.css"],
+})
+export class CourseCardComponent {
+  @Input()
+  course: Course;
+}
